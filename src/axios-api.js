5885e527758aead1427c15261e338bbd3cf394f9
Mark Dziuban <markdziuban@gmail.com>
2021-11-03 13:42:19 -0400

    added env file

--- src/axios-api.js
+++ src/axios-api.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 const getAPI = axios.create({
     // baseURL: 'https://sanctuary-django.herokuapp.com',
-    baseURL: os.environ.get('BASE_URL'),
+    baseURL: process.env.BASE_URL,
     timeout: 1000,
 })
 
